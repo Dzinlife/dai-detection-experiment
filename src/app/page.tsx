d7c5f5cefc54da04d9540f839e7602a4c43fab0a
Wz <dzinlife@me.com>
2024-01-30 05:40:24 +0800

    mfcc + dtw algorithm

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,139 @@
-import Image from "next/image";
+"use client";
+
+import { MouseEventHandler, useRef, useState } from "react";
+import Meyda from "meyda";
+import Dtw from "dynamic-time-warping";
 
 export default function Home() {
+  const inputRef_1 = useRef<HTMLInputElement>(null);
+  const inputRef_2 = useRef<HTMLInputElement>(null);
+
+  function preprocess(audioFile: File): Promise<AudioBuffer> {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onload = async (event) => {
+        if (!event?.target?.result) return;
+        if (typeof event.target.result === "string") return;
+
+        const audioContext = new AudioContext();
+
+        try {
+          const buffer = await audioContext.decodeAudioData(
+            event.target.result
+          );
+          resolve(buffer);
+        } catch (error) {
+          reject(error);
+        }
+      };
+      reader.onerror = function (error) {
+        reject(error);
+      };
+      reader.readAsArrayBuffer(audioFile);
+    });
+  }
+
+  const chunkNum = 300;
+
+  async function extractFeatures(audio: AudioBuffer) {
+    // const hopSize = Math.pow(2, 14); // 16384 ~0.37s
+    // const hopSize = Math.pow(2, 15); // 32768 ~0.75s
+    const hopSize = Math.pow(2, 16); // 65536 ~1.5s
+
+    const semples = audio
+      .getChannelData(0)
+      .slice(hopSize * 0, hopSize * chunkNum);
+
+    const chuckLength = Math.floor(semples.length / hopSize);
+
+    console.log("total chucks num", chuckLength);
+
+    let prevChunk: Float32Array | undefined;
+
+    const features: number[][] = [];
+    for (let i = 0; i < chuckLength; i++) {
+      const chunk = semples.slice(i * hopSize, (i + 1) * hopSize);
+      const mfcc = Meyda.extract("mfcc", chunk) as number[];
+      // prevChunk = chunk;
+      if (!mfcc) continue;
+      // const reduced = (mfcc as number[]).reduce((a, b) => a + b, 0);
+      console.log(i, mfcc);
+      mfcc && features.push(mfcc);
+    }
+    return features;
+  }
+
+  const [path, setPath] = useState<number[][]>();
+
+  const handleClick: MouseEventHandler<HTMLButtonElement> = async () => {
+    const start = Date.now();
+    const file_1 = inputRef_1.current?.files?.[0];
+    if (!file_1) return;
+    const audio_1 = await preprocess(file_1);
+
+    const file_2 = inputRef_2.current?.files?.[0];
+    if (!file_2) return;
+    const audio_2 = await preprocess(file_2);
+
+    console.log("wav convert done, time: " + (Date.now() - start) / 1000 + "s");
+
+    const [mfcc_1, mfcc_2] = await Promise.all([
+      extractFeatures(audio_1),
+      extractFeatures(audio_2),
+    ]);
+
+    console.log(mfcc_1, mfcc_2);
+
+    const dtw = new Dtw(mfcc_1, mfcc_2, (vec1, vec2) => {
+      let sum = 0;
+      for (let i = 0; i < vec1.length; i++) {
+        sum += Math.pow(vec1[i] - vec2[i], 2);
+      }
+      return Math.sqrt(sum);
+    });
+
+    const path = dtw.getPath();
+    console.log(path);
+
+    const mtx = Array(chunkNum)
+      .fill([])
+      .map(() => Array(chunkNum).fill(0));
+
+    path.forEach(([a, b]) => {
+      mtx[a][b] = 1;
+    });
+
+    setPath(mtx);
+
+    const end = Date.now();
+
+    const time = end - start;
+    alert("done, time: " + time / 1000 + "s");
+  };
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <input type="file" ref={inputRef_1} />
+      <input type="file" ref={inputRef_2} />
+      <button className="border px-2 py-1 rounded-md" onClick={handleClick}>
+        Process
+      </button>
+      <table>
+        {path?.map((row, i) => {
+          return (
+            <tr key={i}>
+              {row.map((col, j) => {
+                return (
+                  <td
+                    key={j}
+                    className={`${col === 1 ? "bg-red-500" : "bg-transparent"}`}
+                  ></td>
+                );
+              })}
+            </tr>
+          );
+        })}
+      </table>
     </main>
   );
 }
